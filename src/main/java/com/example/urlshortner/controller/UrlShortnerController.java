064c3bdd2315eddaf0f0ab82a76e60de8235b1a4
Pragya Tyagi <pragyatyagi@Pragyas-MacBook-Air.local>
2024-02-07 18:27:44 +0530

    added get url with and without id

--- src/main/java/com/example/urlshortner/controller/UrlShortnerController.java
+++ src/main/java/com/example/urlshortner/controller/UrlShortnerController.java
@@ -2,14 +2,12 @@ package com.example.urlshortner.controller;
 
 import com.example.urlshortner.interfaces.UrlShortnerService;
 import com.example.urlshortner.model.UrlDao;
+import com.example.urlshortner.model.UrlDto;
 import com.example.urlshortner.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,6 +31,21 @@ public class UrlShortnerController {
 
         ApiResponse apiResponse = new ApiResponse("Success", responseBody);
         return new ResponseEntity<>(apiResponse, HttpStatusCode.valueOf(201));
+    }
+
+    @GetMapping("api/v1/get-short-url")
+    public ResponseEntity getShortUrlById(@RequestParam(required = false) Integer id) throws Exception{
+        Map<String, String> responseBody = new HashMap<>();
+        if(id == null) {
+            return new ResponseEntity<>(new ApiResponse("id required", responseBody), HttpStatusCode.valueOf(200));
+        }
+        UrlDto urlDto = urlShortnerService.getShortUrlById(id);
+
+        responseBody.put("short-url", urlDto.getShortUrl());
+        responseBody.put("original-url", urlDto.getOriginalUrl());
+
+        ApiResponse apiResponse = new ApiResponse("Success", responseBody);
+        return new ResponseEntity<>(apiResponse, HttpStatusCode.valueOf(301));
     }
 
 }
--- src/main/java/com/example/urlshortner/serviceimpl/UrlShortenerServiceImpl.java
+++ src/main/java/com/example/urlshortner/serviceimpl/UrlShortenerServiceImpl.java
@@ -3,10 +3,13 @@ package com.example.urlshortner.serviceimpl;
 import com.example.urlshortner.entity.UrlEntity;
 import com.example.urlshortner.interfaces.UrlShortnerService;
 import com.example.urlshortner.model.UrlDao;
+import com.example.urlshortner.model.UrlDto;
 import com.example.urlshortner.repository.UrlRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -19,6 +22,17 @@ public class UrlShortenerServiceImpl implements UrlShortnerService {
     }
 
     @Override
+    public UrlDto getShortUrlById(Integer id) {
+        Optional<UrlEntity> optionalUrlEntity = urlRepository.findById(id);
+        UrlDto urlDto = new UrlDto();
+        if(optionalUrlEntity.isPresent()) {
+            urlDto.setShortUrl(optionalUrlEntity.get().getShortUrl());
+            urlDto.setOriginalUrl(optionalUrlEntity.get().getOriginalUrl());
+        }
+        return urlDto;
+    }
+
+    @Override
     @Transactional
     public Integer addNewUrlToShorten(UrlDao urlDao) {
         UrlEntity urlEntity = new UrlEntity();
@@ -28,10 +42,11 @@ public class UrlShortenerServiceImpl implements UrlShortnerService {
         try {
             urlRepository.save(urlEntity);
             log.info("data is saved but not persisted");
-//            UrlEntity entity = urlRepository.getReferenceById(urlEntity.getUserId());
         }catch (Exception e) {
             log.error(e.getMessage());
         }
         return urlEntity.getId();
     }
+
+
 }
